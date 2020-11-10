9cd99bd8b836e118d720a3af50e0229f73f68619
agustinasaputra <38872545+agustinartf13@users.noreply.github.com>
2020-11-11 05:18:26 +0800

    slicing home

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDM061PTw27kAWMvZnGFguQI0fniV2D190",
+    authDomain: "clone-f6676.firebaseapp.com",
+    databaseURL: "https://clone-f6676.firebaseio.com",
+    projectId: "clone-f6676",
+    storageBucket: "clone-f6676.appspot.com",
+    messagingSenderId: "428218446525",
+    appId: "1:428218446525:web:8d4dca8293523d0e3c3dd4",
+    measurementId: "G-BNCX6QGNF4"
+};
---
+++ src/utils/StateProvider.js
@@ -0,0 +1,14 @@
+import React, {createContext, useContext, useReducer} from 'react';
+
+// prepare the data layer
+export const StateContext = createContext();
+
+// Wrap our app and provide the data layer
+export const StateProvider = ({ reducer, initialState, children }) => (
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+        {children}
+    </StateContext.Provider>
+);
+
+// pull information from the data layer
+export const useStateValue = () => useContext(StateContext)
